fdb2a8b5aa99313fe78cac6c5f8ea28ae394c414
PedroFontanaMartins <pedrofontanamartins@gmail.com>
2025-05-07 22:26:14 -0300

    Ajuste swagger

--- CafeteriaApi/CafeteriaApi/CafeteriaApi/Program.cs
+++ CafeteriaApi/CafeteriaApi/CafeteriaApi/Program.cs
@@ -4,22 +4,22 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite("Data Source=usuarios.db"));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Cafeteria API V1");
+    c.RoutePrefix = string.Empty; // Swagger na raiz "/"
+});
 
 app.UseHttpsRedirection();
 
