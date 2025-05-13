1357800f38610a60c75e8d34c17098821de42678
PedroFontanaMartins <pedrofontanamartins@gmail.com>
2025-05-12 21:04:22 -0300

    Cors

--- CafeteriaApi/CafeteriaApi/CafeteriaApi/Program.cs
+++ CafeteriaApi/CafeteriaApi/CafeteriaApi/Program.cs
@@ -3,25 +3,36 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// CORS liberado para tudo (apenas para testes!)
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite("Data Source=usuarios.db"));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "Cafeteria API V1");
-    c.RoutePrefix = string.Empty; // Swagger na raiz "/"
+    c.RoutePrefix = string.Empty;
 });
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAll"); // <--- aqui
 
 app.UseAuthorization();
 
