044f821f6f0815d80f96633f99a0f62d2885c3d2
PedroFontanaMartins <148506459+PedroFontanaMartins@users.noreply.github.com>
2025-06-09 00:47:35 -0300

    MERGE INTO MASTER

---
+++ CafeteriaApi/CafeteriaApi/CafeteriaApi/AppDbContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace CafeteriaApi
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+
+            // Use aqui sua connection string local para o PostgreSQL
+            var connectionString = "Host=shuttle.proxy.rlwy.net;Port=37395;Database=railway;Username=postgres;Password=LqRliGLdxrPZRNpiqixWqMKELUSqPxrF";
+
+            optionsBuilder.UseNpgsql(connectionString);
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
