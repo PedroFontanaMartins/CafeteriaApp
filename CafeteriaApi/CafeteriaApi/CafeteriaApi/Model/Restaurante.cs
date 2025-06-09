0ae63236802018fc2ba5cf1c574ade021211c6ed
PedroFontanaMartins <148506459+PedroFontanaMartins@users.noreply.github.com>
2025-06-09 15:44:11 -0300

    MERGE

--- CafeteriaApi/CafeteriaApi/CafeteriaApi/Model/Restaurante.cs
+++ CafeteriaApi/CafeteriaApi/CafeteriaApi/Model/Restaurante.cs
@@ -8,7 +8,6 @@ namespace CafeteriaApi.Model
         public int Id { get; set; }
         public string? Nome { get; set; }
         public string? Foto { get; set; }
-        public string? Endereco { get; set; }
         public decimal? Avaliacao { get; set; }
     }
 }
